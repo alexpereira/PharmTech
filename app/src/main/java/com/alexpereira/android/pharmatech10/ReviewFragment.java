98019d2aae75ae32f3bba0e983d5d8ff69872842
Alex Pereira <alex.pereira@outlook.com>
2016-12-10 22:05:54 -0500

    Adding drug cards to Review

--- app/src/main/java/com/alexpereira/android/pharmatech10/ReviewFragment.java
+++ app/src/main/java/com/alexpereira/android/pharmatech10/ReviewFragment.java
@@ -38,7 +38,7 @@ public class ReviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.recycler_view_frag, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_review, container, false);
         rootView.setTag(TAG);
 
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
